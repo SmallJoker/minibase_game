f84fa850bfc43cd1dc2d22aeb6fabe744a60b559
SmallJoker <mk939@ymail.com>
2014-09-28 14:38:10 +0200

    Clean up bucket mod

--- mods/bucket/init.lua
+++ mods/bucket/init.lua
@@ -1,42 +1,20 @@
 -- Minetest 0.4 mod: bucket
 -- See README.txt for licensing and other information.
 
-local LIQUID_MAX = 8  --The number of water levels when liquid_finite is enabled
-
 minetest.register_alias("bucket", "bucket:bucket_empty")
 minetest.register_alias("bucket_water", "bucket:bucket_water")
 minetest.register_alias("bucket_lava", "bucket:bucket_lava")
 
-minetest.register_craft({
-	output = "bucket:bucket_empty",
-	recipe = {
-		{"default:steel_ingot", "", "default:steel_ingot"},
-		{"", "default:steel_ingot", ""},
-	}
-})
-
-minetest.register_craft({
-	output = "default:lava_source",
-	recipe = {
-		{"default:stick"},
-		{"bucket:bucket_lava"},
-		{"vessels:glass_bottle"},
-	},
-	replacements = { {"bucket:bucket_lava", "bucket:bucket_empty"} },
-})
-
-minetest.register_craft({
-	output = "bucket:bucket_lava",
-	recipe = {
-		{"default:stick"},
-		{"default:lava_source"},
-		{"bucket:bucket_empty"},
-	},
-	replacements = { {"bucket:bucket_empty", "bucket:bucket_lava"} },
-})
-
 bucket = {}
 bucket.liquids = {}
+
+local function check_protection(pos, name, text)
+	if minetest.is_protected(pos, name) then
+		minetest.record_protection_violation(pos, name)
+		return true
+	end
+	return false
+end
 
 -- Register a new liquid
 --   source = name of the source node
@@ -52,76 +30,52 @@ function bucket.register_liquid(source, flowing, itemname, inventory_image, name
 	}
 	bucket.liquids[flowing] = bucket.liquids[source]
 
-	if itemname then return end
+	if not itemname then
+		return
+	end
 	minetest.register_craftitem(itemname, {
 		description = name,
 		inventory_image = inventory_image,
 		stack_max = 1,
 		liquids_pointable = true,
-		groups = {not_in_creative_inventory=1},
 		on_place = function(itemstack, user, pointed_thing)
-			-- Must be pointing to node
 			if pointed_thing.type ~= "node" then
 				return
 			end
 			
-			-- Call on_rightclick if the pointed node defines it
-			if user and not user:get_player_control().sneak then
-				local n = minetest.get_node(pointed_thing.under)
-				local nn = n.name
-				if minetest.registered_nodes[nn] and minetest.registered_nodes[nn].on_rightclick then
-					return minetest.registered_nodes[nn].on_rightclick(pointed_thing.under, n, user, itemstack) or itemstack
-				end
-			end
-			
-
-			local place_liquid = function(pos, user, node, source, flowing, fullness)
-				if not minetest.is_singleplayer() then
-					if pos.y > -5 and source == "default:lava_source" then
-						minetest.chat_send_player(user:get_player_name(), "Do not place lava over -5m, that could end really bad!", true)
-						return
-					end
-				end
-				if minetest.is_protected(pos, user:get_player_name()) then
-					minetest.record_protection_violation(pos, user:get_player_name())
-					return
-				end
-				if math.floor(fullness/128) == 1 or (not minetest.setting_getbool("liquid_finite")) then
-					minetest.add_node(pos, {name=source, param2=fullness})
-					return
-				elseif node.name == flowing then
-					fullness = fullness + node.param2
-				elseif node.name == source then
-					fullness = LIQUID_MAX
-				end
-
-				if fullness >= LIQUID_MAX then
-					minetest.add_node(pos, {name=source, param2=LIQUID_MAX})
-				else
-					minetest.add_node(pos, {name=flowing, param2=fullness})
-				end
-			end
-
-			-- Check if pointing to a buildable node
 			local node = minetest.get_node(pointed_thing.under)
-			local fullness = tonumber(itemstack:get_metadata())
-			if not fullness then fullness = LIQUID_MAX end
+			local ndef = minetest.registered_nodes[node.name]
+			
+			-- Call on_rightclick if the pointed node defines it
+			if ndef.on_rightclick and not user:get_player_control().sneak then
+				return ndef.on_rightclick(
+					pointed_thing.under,
+					node, user,
+					itemstack) or itemstack
+			end
 
-			if minetest.registered_nodes[node.name].buildable_to and not minetest.registered_nodes[node.name].on_rightclick then
-				-- buildable; replace the node
-				place_liquid(pointed_thing.under, user, node, source, flowing, fullness)
+			local place_at = nil
+			if ndef.buildable_to then
+				place_at = pointed_thing.under
 			else
 				-- not buildable to; place the liquid above
 				-- check if the node above can be replaced
-				local node = minetest.get_node(pointed_thing.above)
-				if minetest.registered_nodes[node.name].buildable_to and not minetest.registered_nodes[node.name].on_rightclick then
-					place_liquid(pointed_thing.above, user, node, source, flowing, fullness)
-				else
-					-- do not remove the bucket with the liquid
-					return
+				node = minetest.get_node(pointed_thing.above)
+				ndef = minetest.registered_nodes[node.name]
+				if ndef.buildable_to then
+					place_at = pointed_thing.above
 				end
 			end
-			return {name="bucket:bucket_empty"}
+			
+			if not place_at then
+				return
+			end
+			if check_protection(place_at, user:get_player_name()) then
+				return
+			end
+			
+			minetest.add_node(place_at, {name = source})
+			return {name = "bucket:bucket_empty"}
 		end
 	})
 end
@@ -132,27 +86,37 @@ minetest.register_craftitem("bucket:bucket_empty", {
 	stack_max = 1,
 	liquids_pointable = true,
 	on_use = function(itemstack, user, pointed_thing)
-		-- Must be pointing to node
 		if pointed_thing.type ~= "node" then
 			return
 		end
+		
 		-- Check if pointing to a liquid source
-		node = minetest.get_node(pointed_thing.under)
-		liquiddef = bucket.liquids[node.name]
-		if liquiddef ~= nil and liquiddef.itemname ~= nil and (node.name == liquiddef.source or
-			(node.name == liquiddef.flowing and minetest.setting_getbool("liquid_finite"))) then
+		local pos = pointed_thing.under
+		local node = minetest.get_node(pos)
+		local ldef = bucket.liquids[node.name]
+		
+		if ldef and ldef.itemname and node.name == ldef.source then
+			if check_protection(pos, user:get_player_name()) then
+				return
+			end
 
-			minetest.add_node(pointed_thing.under, {name="air"})
-
-			if node.name == liquiddef.source then node.param2 = LIQUID_MAX end
-			return ItemStack({name = liquiddef.itemname, metadata = tostring(node.param2)})
+			minetest.remove_node(pos)
+			return {name = ldef.itemname}
 		end
 	end,
 })
 
+minetest.register_craft({
+	output = "bucket:bucket_empty",
+	recipe = {
+		{"default:steel_ingot", "", "default:steel_ingot"},
+		{"", "default:steel_ingot", ""},
+	}
+})
+
 bucket.register_liquid(
 	"default:water_source",
-	"default:water_flowing",
+	"",
 	"bucket:bucket_water",
 	"bucket_water.png",
 	"Water Bucket"
@@ -160,7 +124,7 @@ bucket.register_liquid(
 
 bucket.register_liquid(
 	"default:lava_source",
-	"default:lava_flowing",
+	"",
 	"bucket:bucket_lava",
 	"bucket_lava.png",
 	"Lava Bucket"
